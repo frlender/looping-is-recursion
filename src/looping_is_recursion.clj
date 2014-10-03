a107c7333c52ecb7b44398858895ee7bc4683598
frlender <geonann@gmail.com>
2014-10-02 20:53:39 -0500

    first several functions

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,27 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [acc n]
+                 (if (zero? n)
+                   acc
+                   (recur (* acc base) (dec n))))]
+    (helper 1 exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [last-element a-seq]
+                 (if (empty? a-seq)
+                   last-element
+                   (recur (first a-seq) (rest a-seq))))]
+    (helper nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [helper (fn [seq1 seq2]
+                 (cond
+                  (and (empty? seq1) (empty? seq2)) true
+                  (or (empty? seq1) (empty? seq2)) false
+                  (not= (first seq1) (first seq2)) false
+                  :else (recur (rest seq1) (rest seq2))))]
+    (helper seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
   ":(")
